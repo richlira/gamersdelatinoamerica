ef9cf15e471bdb1ca98754749485d7210805d12d
Rich L <ricardodelavega@gmail.com>
2019-11-06 13:52:35 -0800

    Initial Commit

---
+++ Gamers de Latinoamerica/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gamers de Latinoamerica
+//
+//  Created by Ricardo Lira de la Vega on 05/11/19.
+//  Copyright © 2019 Rich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
